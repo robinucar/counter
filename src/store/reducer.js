59bbfb1b97992db2de4b3fef3a844859c6ff0585
Robin <mehmet.ucar@imediapro.co.uk>
2020-04-28 21:12:28 +0100

    react stated deleted and reducer applied

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,31 @@
 const initalState = {
   counter:0
 }
-//reducer
 
 const reducer = (state = initalState, action) => {
+  if(action.type === 'INCREMENT'){
+    return{
+      counter: state.counter + 1
+    }
+  }
 
+  if(action.type === 'DECREMENT'){
+    return{
+      counter: state.counter -1
+    }
+  }
+
+  if(action.type === 'ADD'){
+    return{
+      counter: state.counter + action.value
+    }
+  }
+
+  if(action.type === 'SUBSTRACT'){
+    return{
+      counter: state.counter - action.value
+    }
+  }
   return state
 }
 
