c7e819591beafeec651913bbc3e22ca640f82fd0
Robin <mehmet.ucar@imediapro.co.uk>
2020-05-01 21:32:00 +0100

    applied middleware and redux devtools extenstion

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/results';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
 
 
@@ -14,7 +14,21 @@ const rootReducer = combineReducers({
   ctr: counterReducer,
   rst: resultReducer
 })
-const store = createStore(rootReducer)
+
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware], Dispatching', action);
+      const result = next(action);
+      console.log('[Middleware], next state', store.getState())
+      return result;
+    }
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
